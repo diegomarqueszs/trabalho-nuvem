57dbcc5dbc12fd5381ac4591486251f6a174f823
Diego Marques <diego.marques@sydle.com>
2024-08-11 11:30:28 -0300

    add data-table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import { TableConsumer } from "@/components/table-consumer";
+import { DataTable } from "@/components/data-table";
+import { columns, Consumer } from "./consumer/columns";
 
-export default function Home() {
+async function getConsumers():Promise<Consumer[]> {
+  const res = await fetch('https://66b8b8c83ce57325ac77f1f1.mockapi.io/api/consumers');
+  const data = await res.json();
+  return data;
+}
+
+
+export default async function Home() {
+  const data = await getConsumers();
+
   return (
     <div className="h-screen flex row">
       <div className="flex flex-1">
         <main className="flex-1 p-6">
-          <TableConsumer />
+          <DataTable columns={columns} data={data}/>
         </main>
       </div>
     </div>
